a1b73f5706adf68991144a62e882e2f226135615
robced <117061812+robced@users.noreply.github.com>
2022-12-09 08:37:33 -0800

    fix: accept strings with spaces

--- data-input-output/main.cpp
+++ data-input-output/main.cpp
@@ -17,7 +17,7 @@ int main(){
 
     std::cout << "Please enter your name and age: " << std::endl;
 
-    std::cin >> data_name;
+    std::getline(std::cin,data_name);
     std::cin >> data_age;
 
     std::cout << "Welcome " << data_name << ", you are " << data_age << " years old." << std::endl;
