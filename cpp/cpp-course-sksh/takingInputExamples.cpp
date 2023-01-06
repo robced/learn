4f27f2173d8aff750a60566748f34198dd4c2a97
robced <117061812+robced@users.noreply.github.com>
2023-01-06 15:53:20 -0800

    takingInputExamples

---
+++ cpp/cpp-course-sksh/takingInputExamples.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+
+    float f = .0f;
+    cin>>f;
+    cout<<"You have entered = "<<f<<endl;
+
+    double d = .0;
+    cin>>d;
+    cout<<"You have entered = "<<d<<endl;
+
+    char c;
+    cin>>c;
+    cout<<"You entered = "<<c<<endl;
+
+    return 0;
+}
