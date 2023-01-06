3431007ddb2b84c4d9c02f4b7c0a2942852089c5
robced <117061812+robced@users.noreply.github.com>
2023-01-06 12:10:17 -0800

    addTwoNum

---
+++ cpp/cpp-course-sksh/addTwoNum.cpp
@@ -0,0 +1,21 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+
+    cout << "Calculate the sum of two numbers" << endl;
+    int i;
+    cout << "First Number: ";
+    cin>>i;
+
+    cout << "Second number: ";
+    int j;
+    cin>>j;
+
+    int k = i + j;
+
+    cout << "The sum is: ";
+    cout << k;
+
+    return 0;
+}
